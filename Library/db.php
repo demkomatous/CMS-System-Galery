72c847a9b4973f892cad2a565c29cfc12d1dd333
Matouš Demko <89312130+demkomatous@users.noreply.github.com>
2021-09-19 09:44:20 +0200

    .

--- Library/db.php
+++ Library/db.php
@@ -1,6 +1,6 @@
 <?php
-	define('DB_NAME', 'database');
-	define('DB_USER', 'root');
+	define('DB_NAME', '');
+	define('DB_USER', '');
 	define('DB_PASSWORD', '');
 	define('DB_HOST', '127.0.0.1');
 
